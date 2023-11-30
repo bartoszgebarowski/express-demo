2e240a00e8fd4f2f55392abbcbb10e4092caf8a2
bartoszgebarowski <gebarowski.bartosz@gmail.com>
2023-11-30 21:55:59 +0100

    :package: add cors package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 9001;
 const countryRoute = require("./routes/country.js");
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
